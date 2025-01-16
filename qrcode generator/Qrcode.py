10993887c2ce1096aecc42a439a9fe509d69d5ee
Jimit Purani <103605157+Jimit07@users.noreply.github.com>
2025-01-16 16:57:00 +0530

    Update Qrcode.py

--- qrcode generator/Qrcode.py
+++ qrcode generator/Qrcode.py
@@ -1,6 +1,8 @@
 # create a qr code image for your link
 
 import qrcode as qr
+# Here i have used an youtube link .You can use any other link
 img = qr.make('https://youtu.be/k4y0ZzYw9Yw?si=O4ET1u-CsUF25ma3')
 type(img)  # qrcode.image.pil.PilImage
+# here image is saved as given name in brackets
 img.save("jitubhaikirasoi.png")
